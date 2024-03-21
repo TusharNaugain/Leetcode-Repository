310947f7fbdd5ef38202cd756193342a510c47d9
Tushar Kumar <153719983+TusharNaugain@users.noreply.github.com>
2024-03-21 12:19:37 +0530

    Time: 764 ms (5.26%) | Memory: 45.7 MB (33.27%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,21 +1,22 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int mis = 0;
-        int dup = 0;
-         for( int i = 1 ; i<=nums.length ; i++){
-             int count = 0;
-             for( int j = 0 ; j<nums.length ; j++){
-                 if(nums[j] == i){
-                     count++;
-                 }
-             }
-                 if(count==0){
-                     mis = i;
-                 }else if( count ==2){
-                     dup = i;
-                 }
-             }
-        return new int[]{dup,mis};
-         }
+        int miss = 0;
+        int dup= 0;
+        for(int i = 1 ; i<=nums.length ; i++){
+            int count = 0;
+        for( int j = 0;j<nums.length ; j++){
+        if(nums[j]==i){
+            count++;
+        }
+        }
+        if(count == 0 ){
+            miss = i;
+        }else if(count ==2){
+            dup = i;
+            }
+        }
+        return new int[]{dup,miss};
+    }
+    }
+
     
-}
