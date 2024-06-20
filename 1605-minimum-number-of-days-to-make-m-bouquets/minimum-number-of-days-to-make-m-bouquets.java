22074cb22fcff596c7cfe0ffb074689a8edbf07c
Tushar Kumar <153719983+TusharNaugain@users.noreply.github.com>
2024-06-20 12:57:15 +0530

    Time: 16 ms (95.31%) | Memory: 58.1 MB (30.40%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,41 +1,36 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        // If it's impossible to create m bouquets with k flowers each
-        if (m * k > bloomDay.length) {
-            return -1;
+        if(bloomDay.length < m*k){
+        return -1;
         }
 
-        // Find the minimum and maximum days in bloomDay
-        int high = bloomDay[0];
-        int low = bloomDay[0];
-        for (int i = 0; i < bloomDay.length; i++) {
-            if (bloomDay[i] < low) {
-                low = bloomDay[i];
-            }
-            if (bloomDay[i] > high) {
-                high = bloomDay[i];
-            }
+    int high = bloomDay[0];
+    int low = bloomDay[0];
+    for(int i = 0 ; i<bloomDay.length ; i++){
+        if(low>bloomDay[i]){
+            low = bloomDay[i];
         }
-
-        int res = -1;
-        // Binary search for the minimum number of days
-        while (high >= low) {
-            int mid = (low + high) / 2;
-            if (isPossible(bloomDay, m, k, mid)) {
-                res = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        if(high<bloomDay[i]){
+            high = bloomDay[i];
         }
+    }
+    int res = -1;
+    while(high>=low){
+        int mid = (low+high)/2;
+       if(isPossible(bloomDay, m , k , mid)){
+        res = mid;
+        high = mid-1;
+       }else{
+        low= mid+1;
+       }
+       }
         return res;
     }
 
-    // Helper method to check if it's possible to make m bouquets in mid days
     private boolean isPossible(int[] bloomDay, int m, int k, int mid) {
-        int bouquets = 0;
         int flowers = 0;
-
+        int bouquets = 0;
+    
         for (int day : bloomDay) {
             if (day <= mid) {
                 flowers++;
@@ -51,7 +46,6 @@ class Solution {
                 return true;
             }
         }
-
         return false;
     }
 }
