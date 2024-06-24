f7ef0d87382e7313300317bea2e3bf9baee84882
Tushar Kumar <153719983+TusharNaugain@users.noreply.github.com>
2024-06-24 20:15:15 +0530

    Time: 14 ms (40.65%) | Memory: 56.7 MB (45.81%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
@@ -5,17 +5,17 @@ class Solution {
         int flip = 0;
 
         for (int i = 0; i < n; i++) {
-            // Remove flip end markers that are no longer relevant
             while (!q.isEmpty() && q.peek() < i) {
                 q.poll();
             }
-
+            
             if (q.size() % 2 == nums[i]) {
+                flip++;
+                q.offer(i + k - 1); // Mark the end of flip effect
+            
                 if (i + k - 1 >= n) {
                     return -1; // Flip exceeds bounds of the array
                 }
-                flip++;
-                q.offer(i + k - 1); // Mark the end of flip effect
             }
         }
 
